7e99c966df587bd50363481b1c2a46f2280f3df6
fc-000 <142074177+fc-000@users.noreply.github.com>
2025-04-15 05:34:24 +0800

    inventory exercise

---
+++ inventory.py
@@ -0,0 +1,27 @@
+# fc's attempt
+# You're managing a tech warehouse that tracks gadget serial numbers 📱🔋🖥
+
+# 🎯 Your task:
+# Store the serial numbers of the gadgets in a list
+
+# Print out the lowest (oldest serial) and the highest (newest serial)
+
+
+
+pretend_serial_numbers = [12345, 543, 1232, 17235, 1234]
+
+print("---Tech Gadgets Serial Number---")
+print(f"the lowest serial number is: '{min(pretend_serial_numbers)}'")
+print(f"the newest serial number is: '{max(pretend_serial_numbers)}' ")
+
+
+'''
+Codedex example 
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
+print(min(lego_parts))
+print(max(lego_parts))
+
+
+'''
