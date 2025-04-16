1f6f2149453178ceb638faf204dee4481085def3
fc-000 <142074177+fc-000@users.noreply.github.com>
2025-04-17 06:22:32 +0800

    added bucketList.py n mixtape.py

---
+++ mixtape.py
@@ -0,0 +1,10 @@
+# mixtape exercise
+
+playlist = ["Hooked on the feeling", 
+            "Spirit in the Sky",
+            "Go All The Way",
+            "Moonage Daydream"
+           ]
+
+for song in playlist:
+  print(song)
