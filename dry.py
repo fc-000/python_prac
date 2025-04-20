ef4eda4ec201f58fbd8adef6829815c7325ff62d
fc-000 <142074177+fc-000@users.noreply.github.com>
2025-04-20 17:04:14 +0800

    added dry and fortune cookie exercises

---
+++ dry.py
@@ -0,0 +1,24 @@
+# D.R.Y. = "Dont Repeat Yourself"
+# codedex sample solution
+
+'''
+import random
+
+list_of_fruits = ['apple', 'orange', 'pineapple']
+
+print(max(3,5)) # calculates maximum of two numbers a and b
+
+print(min(-1,7)) # calculates the minimum of two numbers a and b
+
+print(len(list_of_fruits)) #prints the length of a list
+
+print(pow(2,6)) # calculates a to the power b
+
+'''
+
+# experimenting a lil with min and max
+
+num = [1, 2, 3]
+
+print(f"the minimum is: {min(num)}")
+print(f"the maximum is: {max(num)}")
