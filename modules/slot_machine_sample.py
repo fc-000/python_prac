84b8c0838838151fbf0dd04f2d97f79de7cf95a6
fc-000 <142074177+fc-000@users.noreply.github.com>
2025-04-24 14:41:04 +0800

    slot machine sample from codedex

--- modules/slot_machine_sample.py
+++ modules/slot_machine_sample.py
@@ -1,6 +1,8 @@
 # Slot Machine 🎰
 # Codédex
 
+'''
+
 import random
 
 symbols = [
@@ -17,3 +19,41 @@ if (results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️�
  print('Jackpot! 💰')
 else:
  print('Thanks for playing!')
+
+'''
+
+'''
+# Slot Machine 2 🎰
+# Codédex
+
+import random
+
+symbols = [
+  '🍒',
+  '🍇',
+  '🍉',
+  '7️⃣'
+]
+
+def play():
+
+  for i in range(1, 51):    
+    results = random.choices(symbols, k=3)
+    print(f'{results[0]} | {results[1]} | {results[2]}')
+    win = results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣'
+
+    if win:
+      print('Jackpot!!! 💰')
+      break
+    else:
+      results = random.choices(symbols, k=3)
+
+answer = ''
+while answer.upper() != 'N':
+  play()
+  answer = input('Keep playing? (Y/N) ')
+
+print('Thanks for playing!')
+
+
+'''
