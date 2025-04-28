4782cc66a4b9b11c849b2136093e5ec78a59bbd3
fc-000 <142074177+fc-000@users.noreply.github.com>
2025-04-28 15:55:02 +0800

    added sets activity in data structures folder

---
+++ data_structures/sets.py
@@ -0,0 +1,11 @@
+print("---Our favorite fruits!---")
+
+my_fav_fruit = {"watermelon", "mango", "banana"}
+
+bestie_fav_fruit ={"strawberry", "blueberry", "watermelon"}
+
+print("combined favorites:", my_fav_fruit.union(bestie_fav_fruit))
+
+print(f"differences in favorites:", my_fav_fruit.difference(bestie_fav_fruit))
+
+print(f"intersection of favorites:", my_fav_fruit.intersection(bestie_fav_fruit))
